4c8daf50931b4010eb28d302d00ef9fcd385c897
rohit15sharmaa <111563581+rohit15sharmaa@users.noreply.github.com>
2022-09-06 13:32:38 +0530

    add

---
+++ form.js
@@ -0,0 +1,12 @@
+ let tog = document.querySelector('.toggle');
+
+let disp = document.querySelector('.mobo_HOME');
+
+ tog.addEventListener('click', function show(){
+ 
+  
+// disp.style.transform = "translateY(0%)"
+  disp.classList.toggle("demo");
+ })
+
+
